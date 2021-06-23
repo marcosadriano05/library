bd04ba96e487d8f41d6eeb6bcf85de5e6082871d
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-23 20:00:13 -0300

    Migration CreateObras

---
+++ src/database/migrations/1624488487100-CreateObras.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateObras1624488487100 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'obras',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'title',
+            type: 'varchar'
+          },
+          {
+            name: 'publisher',
+            type: 'varchar'
+          },
+          {
+            name: 'photo',
+            type: 'varchar'
+          },
+          {
+            name: 'authors',
+            type: 'varchar[]'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('obras')
+  }
+
+}
