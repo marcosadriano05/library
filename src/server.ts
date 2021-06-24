8a83ded313bb30679eec2e76f64e05c9508b88c4
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-24 15:58:58 -0300

    Add routes to express

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import 'reflect-metadata'
 import express from 'express'
 
 import './database'
+import { route } from './routes'
 
 const server = express()
+server.use(express.json())
+server.use(route)
 
 server.listen(3333, () => console.log('Server on port 3333'))
