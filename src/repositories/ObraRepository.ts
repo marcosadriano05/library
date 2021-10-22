50ac16ceef933f07ac6560a15971072a66f17eda
Marcos Adriano <marcosadriano740@gmail.com>
2021-10-21 23:18:11 -0300

    refactor: removing old code

--- src/repositories/ObraRepository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm'
-import { Obra } from '../entities/Obra'
-
-@EntityRepository(Obra)
-class ObraRepository extends Repository<Obra> {}
-
-export { ObraRepository }
